e2c2effc5735a3046af526f919b877d3eb6cbc94
Rajkumar07793 <rajkumar07793@gmail.com>
2024-11-14 19:10:03 +0530

    * Merged both branch code dev-raj and dev-lakhan * Implemented contact list api and show in contact list * Implemented contact picker and get the contact list * Implemented contact sync api and added permission handler checked the permission granted or not otherwise request * UI corrected bottom navigation bar, app bar, popup menu item etc

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:spam_delection_app/constants/string_constants.dart';
 import 'package:spam_delection_app/globals/app_fonts.dart';
-import 'package:spam_delection_app/screens/profile_screen.dart';
 
 import '../constants/icons_constants.dart';
-import '../constants/image_constants.dart';
 import '../globals/colors.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -25,165 +23,165 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColor.secondryColor,
-      appBar: AppBar(
-          leading: Image.asset(
-            IconConstants.icdrawer,
-            height: MediaQuery.of(context).size.height * 4 / 100,
-          ),
-          title: GestureDetector(
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Profile()));
-            },
-            child: Image.asset(ImageConstants.imageProfile,
-                height: MediaQuery.of(context).size.height * 6 / 100),
-          ),
-          actions: [
-            Image.asset(
-              IconConstants.icNotification,
-              height: MediaQuery.of(context).size.height * 3 / 100,
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 5 / 100,
-            ),
-            GestureDetector(
-              onTap: () {
-                showDialog(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return AlertDialog(
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(8)),
-                        ),
-                        backgroundColor: AppColor.callUpdateColor,
-                        content: Align(
-                          alignment: Alignment.topRight,
-                          child: SizedBox(
-                            height:
-                                MediaQuery.of(context).size.height * 40 / 100,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      3 /
-                                      100,
-                                ),
-                                Row(
-                                  children: [
-                                    Image.asset(IconConstants.icOutgoing),
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          5 /
-                                          100,
-                                    ),
-                                    const Text(
-                                      'Outgoing Calls',
-                                      style: TextStyle(
-                                          color: Colors.black,
-                                          fontSize: 18,
-                                          fontWeight: FontWeight.w600),
-                                    )
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      4 /
-                                      100,
-                                ),
-                                Row(
-                                  children: [
-                                    Image.asset(
-                                      IconConstants.icIncoming,
-                                    ),
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          5 /
-                                          100,
-                                    ),
-                                    const Text('Incomig Calls',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w600))
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      4 /
-                                      100,
-                                ),
-                                Row(
-                                  children: [
-                                    Image.asset(IconConstants.icMissedCall),
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          5 /
-                                          100,
-                                    ),
-                                    const Text('Missed Calls',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w600))
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      4 /
-                                      100,
-                                ),
-                                Row(
-                                  children: [
-                                    Image.asset(IconConstants.icBlockCall),
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          5 /
-                                          100,
-                                    ),
-                                    const Text('Blocked Calls',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w600))
-                                  ],
-                                ),
-                                SizedBox(
-                                  height: MediaQuery.of(context).size.height *
-                                      4 /
-                                      100,
-                                ),
-                                Row(
-                                  children: [
-                                    Image.asset(IconConstants.icDelete),
-                                    SizedBox(
-                                      width: MediaQuery.of(context).size.width *
-                                          5 /
-                                          100,
-                                    ),
-                                    const Text('Delete all Calls',
-                                        style: TextStyle(
-                                            color: Colors.black,
-                                            fontSize: 18,
-                                            fontWeight: FontWeight.w600))
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    });
-              },
-              child: Image.asset(
-                IconConstants.icMoreDetails,
-                height: MediaQuery.of(context).size.height * 3 / 100,
-              ),
-            ),
-            SizedBox(
-              width: MediaQuery.of(context).size.width * 6 / 100,
-            ),
-          ]),
+      // appBar: AppBar(
+      //     leading: Image.asset(
+      //       IconConstants.icdrawer,
+      //       height: MediaQuery.of(context).size.height * 4 / 100,
+      //     ),
+      //     title: GestureDetector(
+      //       onTap: () {
+      //         Navigator.push(context,
+      //             MaterialPageRoute(builder: (context) => const Profile()));
+      //       },
+      //       child: Image.asset(ImageConstants.imageProfile,
+      //           height: MediaQuery.of(context).size.height * 6 / 100),
+      //     ),
+      //     actions: [
+      //       Image.asset(
+      //         IconConstants.icNotification,
+      //         height: MediaQuery.of(context).size.height * 3 / 100,
+      //       ),
+      //       SizedBox(
+      //         width: MediaQuery.of(context).size.width * 5 / 100,
+      //       ),
+      //       GestureDetector(
+      //         onTap: () {
+      //           showDialog(
+      //               context: context,
+      //               builder: (BuildContext context) {
+      //                 return AlertDialog(
+      //                   shape: const RoundedRectangleBorder(
+      //                     borderRadius: BorderRadius.all(Radius.circular(8)),
+      //                   ),
+      //                   backgroundColor: AppColor.callUpdateColor,
+      //                   content: Align(
+      //                     alignment: Alignment.topRight,
+      //                     child: SizedBox(
+      //                       height:
+      //                           MediaQuery.of(context).size.height * 40 / 100,
+      //                       child: Column(
+      //                         crossAxisAlignment: CrossAxisAlignment.start,
+      //                         children: [
+      //                           SizedBox(
+      //                             height: MediaQuery.of(context).size.height *
+      //                                 3 /
+      //                                 100,
+      //                           ),
+      //                           Row(
+      //                             children: [
+      //                               Image.asset(IconConstants.icOutgoing),
+      //                               SizedBox(
+      //                                 width: MediaQuery.of(context).size.width *
+      //                                     5 /
+      //                                     100,
+      //                               ),
+      //                               const Text(
+      //                                 'Outgoing Calls',
+      //                                 style: TextStyle(
+      //                                     color: Colors.black,
+      //                                     fontSize: 18,
+      //                                     fontWeight: FontWeight.w600),
+      //                               )
+      //                             ],
+      //                           ),
+      //                           SizedBox(
+      //                             height: MediaQuery.of(context).size.height *
+      //                                 4 /
+      //                                 100,
+      //                           ),
+      //                           Row(
+      //                             children: [
+      //                               Image.asset(
+      //                                 IconConstants.icIncoming,
+      //                               ),
+      //                               SizedBox(
+      //                                 width: MediaQuery.of(context).size.width *
+      //                                     5 /
+      //                                     100,
+      //                               ),
+      //                               const Text('Incomig Calls',
+      //                                   style: TextStyle(
+      //                                       color: Colors.black,
+      //                                       fontSize: 18,
+      //                                       fontWeight: FontWeight.w600))
+      //                             ],
+      //                           ),
+      //                           SizedBox(
+      //                             height: MediaQuery.of(context).size.height *
+      //                                 4 /
+      //                                 100,
+      //                           ),
+      //                           Row(
+      //                             children: [
+      //                               Image.asset(IconConstants.icMissedCall),
+      //                               SizedBox(
+      //                                 width: MediaQuery.of(context).size.width *
+      //                                     5 /
+      //                                     100,
+      //                               ),
+      //                               const Text('Missed Calls',
+      //                                   style: TextStyle(
+      //                                       color: Colors.black,
+      //                                       fontSize: 18,
+      //                                       fontWeight: FontWeight.w600))
+      //                             ],
+      //                           ),
+      //                           SizedBox(
+      //                             height: MediaQuery.of(context).size.height *
+      //                                 4 /
+      //                                 100,
+      //                           ),
+      //                           Row(
+      //                             children: [
+      //                               Image.asset(IconConstants.icBlockCall),
+      //                               SizedBox(
+      //                                 width: MediaQuery.of(context).size.width *
+      //                                     5 /
+      //                                     100,
+      //                               ),
+      //                               const Text('Blocked Calls',
+      //                                   style: TextStyle(
+      //                                       color: Colors.black,
+      //                                       fontSize: 18,
+      //                                       fontWeight: FontWeight.w600))
+      //                             ],
+      //                           ),
+      //                           SizedBox(
+      //                             height: MediaQuery.of(context).size.height *
+      //                                 4 /
+      //                                 100,
+      //                           ),
+      //                           Row(
+      //                             children: [
+      //                               Image.asset(IconConstants.icDelete),
+      //                               SizedBox(
+      //                                 width: MediaQuery.of(context).size.width *
+      //                                     5 /
+      //                                     100,
+      //                               ),
+      //                               const Text('Delete all Calls',
+      //                                   style: TextStyle(
+      //                                       color: Colors.black,
+      //                                       fontSize: 18,
+      //                                       fontWeight: FontWeight.w600))
+      //                             ],
+      //                           ),
+      //                         ],
+      //                       ),
+      //                     ),
+      //                   ),
+      //                 );
+      //               });
+      //         },
+      //         child: Image.asset(
+      //           IconConstants.icMoreDetails,
+      //           height: MediaQuery.of(context).size.height * 3 / 100,
+      //         ),
+      //       ),
+      //       SizedBox(
+      //         width: MediaQuery.of(context).size.width * 6 / 100,
+      //       ),
+      //     ]),
       // body: _pages[_page],
       body: SafeArea(
         child: SingleChildScrollView(
